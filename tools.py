ce3fae92e278ee9ff1639865f9a7526590c9b791
Vladimir Sp <trokbrok@yandex.by>
2022-01-07 02:13:59 +0300

    Init tools methods.

---
+++ tools.py
@@ -0,0 +1,36 @@
+"""
+Helper tools for operations
+"""
+
+
+class Basket:
+    """
+    Helper tools for transfer product from Dozatora to Oven
+    """
+    def __init__(self):
+        pass
+
+    def parking(self):
+        """
+        After every operation tools must be put to holder palace
+        """
+        pass
+
+
+class Spatula:
+    """
+    Main tools for cooking
+    """
+    def __init__(self):
+        pass
+
+    def parking(self):
+        """
+        After every operation tools must be put to holder palace
+        """
+        pass
+
+    def rotate(self):
+        """
+        Get product(s) and rotate him
+        """
